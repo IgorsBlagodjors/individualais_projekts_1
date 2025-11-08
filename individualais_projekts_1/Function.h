17b3a0111203d2d501dbc0ea8192f333cac276a2
IgorsBlagodjors <igor.blagogo@gmail.com>
2025-11-08 20:46:30 +0200

    update

--- individualais_projekts_1/Function.h
+++ individualais_projekts_1/Function.h
@@ -10,12 +10,9 @@ void saveEmployees(const Employee *arr, int size, const char *filename);
 void saveDiscountCards(const DiscountCard *arr, int size, const char *filename);
 void saveReceipts(const Receipt *arr, int size, const char *filename);
 
-void loadProducts(const char *filename, Product *&arr, int &size,
-                  int &capacity);
-void loadEmployees(const char *filename, Employee *&arr, int &size,
-                   int &capacity);
-void loadDiscountCards(const char *filename, DiscountCard *&arr, int &size,
-                       int &capacity);
+void loadProducts(const char *filename, Product *&arr, int &size, int &capacity);
+void loadEmployees(const char *filename, Employee *&arr, int &size, int &capacity);
+void loadDiscountCards(const char *filename, DiscountCard *&arr, int &size, int &capacity);
 void loadReceipts(const char *filename, Receipt *&arr, int &size,
                   Product *products, int productSize, DiscountCard *cards,
                   int cardSize, int &capacity);
